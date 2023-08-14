2b8749ecf142f67c03a5c99d3c00ebead6018aa8
Fernando T. Torales Acosta <ftoralesacosta@ml4hep1.lbl.gov>
2023-08-14 09:47:34 -0700

    running updates

---
+++ refactor_unfolding.py
@@ -0,0 +1,158 @@
+import sys
+import numpy as np
+import pandas as pd
+# import matplotlib.pyplot as plt
+import gc
+import time
+import os
+
+import tensorflow as tf
+import tensorflow.keras
+
+from unfold import multifold
+
+from get_np_arrays import get_kinematics
+
+'''
+run like python refactor_unfolding.py Rapgap nominal  0
+[command] [Rapgap or Django] [run_type: nominal, sys_0...] [BOOTSTRAPPING Seed]
+'''
+
+print("Running on MC sample", sys.argv[1], "with setting", sys.argv[2])
+
+# os.environ['CUDA_VISIBLE_DEVICES'] = "0"
+# os.environ['CUDA_VISIBLE_DEVICES'] = "1"
+os.environ['CUDA_VISIBLE_DEVICES'] = "2"
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+# gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.4)
+# session = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+
+# LABEL = "npyFile_Iter30"
+LABEL = "Aug2_unfoldpy"
+save_dir = "/global/ml4hep/spss/ftoralesacosta/new_models"
+ID = f"{sys.argv[1]}_{sys.argv[2]}_{LABEL}"
+print(f"\n\n ID = {ID} \n\n")
+
+
+# tf.random.set_seed(int(sys.argv[3]))
+np.random.seed(int(sys.argv[3]))
+
+test = False
+add_asymmetry = False
+leading_jets_only = True
+num_observables = 8
+
+NEVENTS = -1
+# NEVENTS = 1_000_000
+n_epochs = 2000
+NIter = 15
+NPasses = 5
+
+if test:
+    NEVENTS = 1_000_000
+    n_epochs = 200
+    NIter = 2
+    NPasses = 2
+
+inputs_dir = "/clusterfs/ml4hep/yxu2/unfolding_mc_inputs"
+mc = pd.read_pickle(f"{inputs_dir}/{sys.argv[1]}_{sys.argv[2]}.pkl")[:NEVENTS]
+data = pd.read_pickle(f"{inputs_dir}/Data_nominal.pkl")[:NEVENTS]
+
+print(f"MC = {inputs_dir}/{sys.argv[1]}_{sys.argv[2]}.pkl")
+print(f"Data = {inputs_dir}/Data_nominal.pkl")
+
+print(np.shape(mc))
+print(np.shape(data))
+
+if (leading_jets_only):
+    njets_tot = len(data["e_px"])
+    data = data.loc[(slice(None), 0), :]
+    mc = mc.loc[(slice(None), 0), :]
+    print("Number of subjets cut = ",
+          njets_tot-len(data["e_px"]), " / ",
+          len(data["jet_pt"]))
+
+reco_vars = ['e_px', 'e_py', 'e_pz',
+             'jet_pt', 'jet_eta', 'jet_phi',
+             'jet_dphi', 'jet_qtnorm']
+
+gen_vars = ['gene_px', 'gene_py', 'gene_pz',
+            'genjet_pt', 'genjet_eta', 'genjet_phi',
+            'genjet_dphi', 'genjet_qtnorm']
+
+# Load the Data
+theta_unknown_S = data[reco_vars].to_numpy()
+theta0_S = mc[reco_vars].to_numpy()
+theta0_G = mc[gen_vars].to_numpy()
+
+weights_MC_sim = mc['wgt']
+pass_reco = np.array(mc['pass_reco'])
+pass_truth = np.array(mc['pass_truth'])
+pass_fiducial = np.array(mc['pass_fiducial'])
+
+print("THE LENGTH OF THE ARRAYS IS =", len(pass_fiducial))
+
+print(f"\n\n SHAPE OF theta_unknown_S {np.shape(theta_unknown_S)} \n\n")
+print(f"\n\n SHAPE OF theta0_S {np.shape(theta0_S)} \n\n")
+
+for ivar in range(8):
+    print(f"theta0_S Variable {reco_vars[ivar]} = ", theta0_S[pass_reco == 1][:5, ivar])
+print()
+for ivar in range(8):
+    print(f"theta0_S Variable (Truth) {gen_vars[ivar]} = ", theta0_S[pass_truth == 1][:5, ivar])
+print()
+for ivar in range(8):
+    print(f"theta0_G Variable (Truth) {gen_vars[ivar]} = ", theta0_G[pass_truth == 1][:5, ivar])
+
+# Add directly the asymmetry angle to the unfolding.
+if add_asymmetry:
+
+    num_observables = 11
+
+    asymm_kinematics = np.asarray(get_kinematics(theta_unknown_S)).T[:, :-1]
+    theta_unknown_S = np.append(theta_unknown_S, asymm_kinematics, 1)
+
+    Sasymm_kinematics = np.asarray(get_kinematics(theta0_S)).T[:, :-1]
+    theta0_S = np.append(theta0_S, Sasymm_kinematics, 1)
+
+    Gasymm_kinematics = np.asarray(get_kinematics(theta0_G)).T[:, :-1]
+    theta0_G = np.append(theta0_G, Gasymm_kinematics, 1)
+
+    print(f"\n\n SHAPE OF theta0_S {np.shape(theta0_S)} \n\n")
+
+    for ivar in range(11):
+        print(f"theta0_S Variable {ivar} = ",
+              theta0_S[pass_reco == 1][:5, ivar])
+    print()
+
+    for ivar in range(11):
+        print(f"theta0_S Variable (Truth) {ivar} = ",
+              theta0_S[pass_truth == 1][:5, ivar])
+    print()
+
+    for ivar in range(11):
+        print(f"theta0_G Variable (Truth) {ivar} = ",
+              theta0_G[pass_truth == 1][:5, ivar])
+
+#  sys.exit("\n\nEXITING\n\n")
+
+theta0_S[:, 0][pass_reco == 0] = -10
+theta0_G[:, 0][pass_truth == 0] = -10
+
+del mc
+gc.collect()
+
+for p in range(NPasses):
+    start = time.time()
+    print(f"Unfolding Pass {p}...")
+
+    weights, models, history = multifold(num_observables, NIter,
+                                         theta0_G, theta0_S,
+                                         theta_unknown_S, n_epochs)
+
+    tf.keras.backend.clear_session()
+    np.save(f"{save_dir}/{ID}/{ID}_Pass{p}_Step2_Weights.npy", weights[:, 1:2, :])
+
+    print(f"Pass {p} took {time.time() - start} seconds \n")
