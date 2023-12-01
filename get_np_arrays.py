944f7e835c9a085144d96080c49a28d8be8b8253
Fernando T. Torales Acosta <ftoralesacosta@ml4hep1.lbl.gov>
2023-11-30 19:08:17 -0800

    added utility functions

--- get_np_arrays.py
+++ get_np_arrays.py
@@ -1,15 +1,15 @@
 import sys
-
 import numpy as np
 import pandas as pd
 import gc
-import time
+import uproot as ur
+import yaml
+# import time
 # import tensorflow as tf
 # from sklearn.preprocessing import StandardScaler
-import h5py
+# import h5py
 # from unfold import weighted_binary_crossentropy
 
-import uproot as ur
 
 # 1 make sure q2 is obtained
 # 2 load nn weights from new npy files
@@ -18,34 +18,54 @@ import uproot as ur
 # 5 save np files with label from mc_name
 
 
-def npy_from_pkl(label, load_NN=True, pkl_path="", mc="Rapgap", type="nominal", keys=[]):
+def npy_from_pkl(label, suffix="", load_NN=True, pkl_path="",
+                 mc="Rapgap", run_type="nominal", keys=[]):
 
     if (pkl_path == ""):
-        pkl_path = "/pscratch/sd/f/fernando/h1_data"
+        # pkl_path = "/pscratch/sd/f/fernando/h1_data"
+        pkl_path = "/clusterfs/ml4hep/yxu2/unfolding_mc_inputs/"
 
-    #DATA LOADING
+    # DATA LOADING
     if ".pkl" in pkl_path:
-        print("Loading PKL directly from argument: ",pkl_path)
+        print("Loading PKL directly from argument: ", pkl_path)
         mc = pd.read_pickle(pkl_path)
 
     else:
-        print("Loading PKL ", f"{pkl_path}/{mc}_{type}.pkl")
-        mc = pd.read_pickle(f"{pkl_path}/{mc}_{type}.pkl") 
-
+        print("Loading PKL ", f"{pkl_path}/{mc}_{run_type}.pkl")
+        mc = pd.read_pickle(f"{pkl_path}/{mc}_{run_type}.pkl")
 
     print("MC SHAPE = ", np.shape(mc))
     leading_jets_only = True
     if (leading_jets_only):
         njets_tot = len(mc["e_px"])
-        mc = mc.loc[(slice(None),0), :]
-        print("Number of subjets cut = ",njets_tot-len(mc["jet_pt"])," / ",len(mc["jet_pt"]))
+        mc = mc.loc[(slice(None), 0), :]
+        print("Number of subjets cut = ",
+              njets_tot-len(mc["jet_pt"]), " / ", len(mc["jet_pt"]))
 
     print("MC SHAPE = ", np.shape(mc))
     if not keys:
-        keys = ['gene_px','gene_py','gene_pz','genjet_pt','genjet_eta',
-                   'genjet_phi','genjet_dphi','genjet_qtnorm', 'gen_Q2']
+        keys = ['gene_px', 'gene_py', 'gene_pz', 'genjet_pt', 'genjet_eta',
+                'genjet_phi', 'genjet_dphi', 'genjet_qtnorm', 'gen_Q2']
+
+    # print("First 5 Events = ", mc.head(5))
+    # print("Last 5 Events = ", mc.tail(5))
+
+    print("*"*30)
+    print("Current Dir = ", sys.path[0])
+    # home = '/clusterfs/ml4hep_nvme2/ftoralesacosta/h1_models'
+    home = '/global/ml4hep/spss/ftoralesacosta/h1_models'
+    NN_step2_weights = np.load(f"{home}/{label}/{label}_Pass0_Step2_Weights.npy")[-1,0]
+    print("WEIGHTS SHAPE = ", np.shape(NN_step2_weights))
+
+    # Ensure lengths are the same.
+    # Was previously checked, if mismatch, the TAIL is
+    # cut. So taking the first N events matches eventns to weights
+    NEVENTS = min(len(NN_step2_weights), len(mc))
+    NN_step2_weights = NN_step2_weights[:NEVENTS]
+    mc = mc[:NEVENTS]
 
     theta0_G = mc[keys].to_numpy()
+
     print("shape of theta0_G = ", np.shape(theta0_G) )
     weights_MC_sim = mc['wgt']
     pass_reco = np.array(mc['pass_reco'])
@@ -54,18 +74,17 @@ def npy_from_pkl(label, load_NN=True, pkl_path="", mc="Rapgap", type="nominal", 
     del mc
     _ = gc.collect()
 
+    # home = '/pscratch/sd/f/fernando/h1_models'
 
-    home = '/pscratch/sd/f/fernando/h1_models'
     # NN_step2_weights = np.load(f"../weights/{label}_pass_avgs.npy")[-1]
     # NN_step2_weights = np.load(f"../h1_models/{label}/{label}_Pass0_Step2_Weights.npy")[-1]
-    NN_step2_weights = np.load(f"{home}/{label}/{label}_Pass0_Step2_Weights.npy")[-1,0]
+
     # Q^2 Cut already applied to NN weights in unfolding procedure!!!
 
     # NN_step2_weights = np.load(f"/pscratch/sd/f/fernando/h1_models/Rapgap_nominal_Rapgap_HVDRapgap_Sep25/Rapgap_nominal_Rapgap_HVDRapgap_Sep25_Pass0_Step2_Weights.npy")[-1,0]
     # NN_step2_weights = np.load(f"/pscratch/sd/f/fernando/h1_models/Rapgap_nominal_Rapgap_HVDRapgap_NCCL_Oct17/Rapgap_nominal_Rapgap_HVDRapgap_NCCL_Oct17_Pass0_Step2_Weights.npy")[-1,0]
 
     # take the last iteration, MultiFold learns from SCRATCH here
-    print("WEIGHTS SHAPE = ", np.shape(NN_step2_weights))
 
     # KINEMATICS
     q_perp_mag, jet_pT_mag, asymm_phi, jet_qT_norm, Q2 = get_kinematics(theta0_G)
@@ -75,19 +94,26 @@ def npy_from_pkl(label, load_NN=True, pkl_path="", mc="Rapgap", type="nominal", 
     cuts = get_cuts(pass_fiducial, pass_truth, q_perp_mag, jet_pT_mag, 
                     asymm_phi, jet_qT_norm, Q2)
 
+    apply_Q2_cut = True
+    if (apply_Q2_cut):
+        Q2 = np.ones(len(q_perp_mag), dtype=bool)
+    else:
+        label += "NO_Q2_CUT_APPLIED"
+
     print("Cuts SHAPE = ", np.shape(cuts))
     weights = weights_MC_sim[cuts] 
     print("SHAPE after Cuts = ", np.shape(weights))
-    weights *= NN_step2_weights
+    weights *= NN_step2_weights[cuts]
 
-    np.save('npy_files/'+label+'_cuts.npy',cuts)
-    np.save('npy_files/'+label+'_jet_pT.npy',jet_pT_mag)
-    np.save('npy_files/'+label+'_q_perp.npy',q_perp_mag)
-    np.save('npy_files/'+label+'_Q2.npy',Q2)
-    np.save('npy_files/'+label+'_asymm_angle.npy',asymm_phi)
-    np.save('npy_files/'+label+'_weights.npy',weights)
-    np.save('npy_files/'+label+'_nn_weights.npy',NN_step2_weights)
-    np.save('npy_files/'+label+'_mc_weights.npy',weights_MC_sim)
+    npy_dir = '/global/ml4hep/spss/ftoralesacosta/h1_asymmetry/npy_files/'
+    np.save(f'{npy_dir}/{label}_cuts.npy', cuts)
+    np.save(f'{npy_dir}/{label}_jet_pT.npy', jet_pT_mag)
+    np.save(f'{npy_dir}/{label}_q_perp.npy', q_perp_mag)
+    np.save(f'{npy_dir}/{label}_Q2.npy', Q2)
+    np.save(f'{npy_dir}/{label}_asymm_angle.npy', asymm_phi)
+    np.save(f'{npy_dir}/{label}_weights.npy', weights)
+    np.save(f'{npy_dir}/{label}_nn_weights.npy', NN_step2_weights)
+    np.save(f'{npy_dir}/{label}_mc_weights.npy', weights_MC_sim)
 
 
 #Primarily for Loading ROOT files, e.g. PYTHIA
@@ -176,11 +202,11 @@ def get_kinematics(theta0_G):
 def get_cuts(pass_fiducial, pass_truth, q_perp_mag, jet_pT_mag, asymm_phi, jet_qT, genQ2=None):
     print("Getting Cut Mask")
 
-    # pT_cut = jet_pT_mag > 10.
-    pT_cut = jet_pT_mag > 20. #Test only for Feb 17
+    pT_cut = jet_pT_mag > 10.
+    # pT_cut = jet_pT_mag > 20. #Test only for Feb 17
     q_over_pT_cut = q_perp_mag/jet_pT_mag < 0.3 #Kyle guessed ~0.3, may need variation
-    # qT_cut = np.where((jet_qT<0.25), True, False)
-    qT_cut = jet_qT < 0.25
+    qT_cut = np.where((jet_qT<0.25), True, False)
+    # qT_cut = jet_qT < 0.25
     phi_nan_cut = ~np.isnan(asymm_phi)
     # print("PHI Not NaN",np.any(phi_nan_cut))
 
@@ -192,7 +218,7 @@ def get_cuts(pass_fiducial, pass_truth, q_perp_mag, jet_pT_mag, asymm_phi, jet_q
     cut_arrays = [
         Q2_cut,
         jet_pT_mag_nan,
-        pass_truth,
+        # pass_truth,
         pass_fiducial,
         pT_cut,
         q_over_pT_cut,
@@ -217,3 +243,21 @@ def get_cuts(pass_fiducial, pass_truth, q_perp_mag, jet_pT_mag, asymm_phi, jet_q
         cuts = np.logical_and(cuts, Q2_cut)
 
     return cuts
+
+
+if __name__ == '__main__':
+
+    CONFIG_FILE = "./config.yaml"
+    config = yaml.safe_load(open(CONFIG_FILE))
+    print(f"\nLoaded {CONFIG_FILE}\n")
+
+    mc = config['mc']  # Rapgap, Django, Pythia
+    run_type = config['run_type']  # nominal, bootstrap, systematic
+    processed_dir = config['main_dir']
+    run_identifier = config['identifier']
+
+    LABEL = f"{mc}_{run_type}_{mc}_{run_identifier}"
+
+    # Rapgap_nominal_Rapgap_SingleWorker_Q2_Cut
+    npy_from_pkl(LABEL, mc=mc)
+    print("Calculating Kinematics")
---
+++ process_functions.py
@@ -0,0 +1,168 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import pickle
+
+
+def averages_in_qperp_bins(dic, q_perp_bins, q_perp, asymm_phi,
+                           weights, print_keys=False):
+
+    digits = np.digitize(q_perp, q_perp_bins)-1
+    N_Bins = len(q_perp_bins)-1
+
+    q_w    = q_perp*weights
+    phi_w  = asymm_phi*weights
+    cos1_w = np.cos(1*asymm_phi)*weights
+    sin1_w = np.sin(1*asymm_phi)*weights
+    cos2_w = np.cos(2*asymm_phi)*weights
+    cos3_w = np.cos(3*asymm_phi)*weights
+
+    q_avg = np.zeros(N_Bins)
+    phi_avg = np.zeros(N_Bins)
+    cos1_avg = np.zeros(N_Bins)
+    sin1_avg = np.zeros(N_Bins)
+    cos2_avg = np.zeros(N_Bins)
+    cos3_avg = np.zeros(N_Bins)
+
+    for i in range(N_Bins):
+        bin_mask = digits == i
+        bin_wsum = np.nansum(weights[bin_mask])
+
+        q_avg[i] = np.nansum(q_w[bin_mask])/bin_wsum
+        phi_avg[i] = np.nansum(phi_w[bin_mask])/bin_wsum
+        cos1_avg[i] = np.nansum(cos1_w[bin_mask])/bin_wsum
+        sin1_avg[i] = np.nansum(sin1_w[bin_mask])/bin_wsum
+        cos2_avg[i] = np.nansum(cos2_w[bin_mask])/bin_wsum
+        cos3_avg[i] = np.nansum(cos3_w[bin_mask])/bin_wsum
+
+    dic["q_perp"] = q_avg
+    dic["phi"] = phi_avg
+    dic["cos1"] = cos1_avg
+    dic["cos2"] = cos2_avg
+    dic["sin1"] = sin1_avg
+    dic["cos3"] = cos3_avg
+
+    if (print_keys):
+        print("Keys =", dic.keys(), "N_Bins = ", N_Bins)
+
+    return
+
+
+
+def get_bootstrap_errors(boot_ensemble,q_perp,q_perp_bins,asymm_phi,cuts,jetpT,title=""):
+    #takes q_perp and and phi, and calcs weighted <cos(n*phi)> for all ensemble iterations
+
+    N_Bootstraps = np.shape(boot_ensemble)[0]
+    N_Bins = len(q_perp_bins)-1
+    digits = np.digitize(q_perp,q_perp_bins)-1
+
+    q_avg = np.zeros((N_Bootstraps,N_Bins))
+    phi_avg = np.zeros((N_Bootstraps,N_Bins))
+    cos1_avg = np.zeros((N_Bootstraps,N_Bins))
+    cos2_avg = np.zeros((N_Bootstraps,N_Bins))
+    cos3_avg = np.zeros((N_Bootstraps,N_Bins))
+    jetpT_avg = np.zeros((N_Bootstraps,N_Bins))
+
+    #Just Used for Plotting:
+    fig,axes = plt.subplots(2,3,figsize=(16,9))
+    q_centers = (q_perp_bins[:-1]+q_perp_bins[1:])/2.0
+    q_width = (q_perp_bins[1]+q_perp_bins[0])/2
+    axes = np.ravel(axes)
+
+    for istrap in tqdm(range(N_Bootstraps)):
+
+        weights=boot_ensemble[istrap][cuts] #This is what fundamentally changes per iteration
+
+        q_w = q_perp*weights
+        phi_w = np.cos(asymm_phi)*weights
+        cos1_w = np.cos(1*asymm_phi)*weights
+        cos2_w = np.cos(2*asymm_phi)*weights
+        cos3_w = np.cos(3*asymm_phi)*weights
+        jetpT_w = jetpT*weights
+
+
+        for i in range(N_Bins):
+            bin_mask = digits==i
+            bin_wsum = np.sum(weights[bin_mask])
+            
+            q_avg[istrap,i]    = np.nansum(q_w[bin_mask])/bin_wsum
+            phi_avg[istrap,i]  = np.nansum(asymm_phi[bin_mask])/bin_wsum
+            cos1_avg[istrap,i] = np.nansum(cos1_w[bin_mask])/bin_wsum
+            cos2_avg[istrap,i] = np.nansum(cos2_w[bin_mask])/bin_wsum
+            cos3_avg[istrap,i] = np.nansum(cos3_w[bin_mask])/bin_wsum
+            jetpT_avg[istrap,i]    = np.nansum(jetpT_w[bin_mask])/bin_wsum
+
+        axes[0].errorbar(q_centers,phi_avg[istrap],xerr=q_width,alpha=0.2)
+        axes[1].errorbar(q_centers,cos1_avg[istrap],xerr=q_width,alpha=0.2)
+        axes[2].errorbar(q_centers,cos2_avg[istrap],xerr=q_width,alpha=0.2)
+        axes[3].errorbar(q_centers,cos3_avg[istrap],xerr=q_width,alpha=0.2)
+        axes[4].errorbar(q_centers,jetpT_avg[istrap],xerr=q_width,alpha=0.2)
+        axes[4].errorbar(q_centers,jetpT_avg[istrap],xerr=q_width,alpha=0.2)
+
+        axes[0].set_ylabel("$\phi$")
+        axes[1].set_ylabel("$\cos(\phi)$")
+        axes[2].set_ylabel("$\cos(2\phi)$")
+        axes[3].set_ylabel("$\cos(3\phi)$")
+        axes[4].set_ylabel("$\mathrm{jet}\ p_\mathrm{T} [\mathrm{GeV}]$")
+
+        axes[0].set_xlabel("$q_\perp$")
+        axes[1].set_xlabel("$q_\perp$")
+        axes[2].set_xlabel("$q_\perp$")
+        axes[3].set_xlabel("$q_\perp$")
+        axes[4].set_xlabel("$\mathrm{jet}\ q_\perp$")
+
+    plt.suptitle(title)
+    plt.tight_layout()
+    plt.savefig("Bootstrap_Ensemble.pdf")
+
+    q_errors = np.zeros(N_Bins)
+    phi_errors = np.zeros(N_Bins)
+    cos1_errors = np.zeros(N_Bins)
+    cos2_errors = np.zeros(N_Bins)
+    cos3_errors = np.zeros(N_Bins)
+
+    for ibin in range(N_Bins):
+        # q_errors[ibin] = np.nanstd(q_avg[:,ibin])/np.nanmean(q_avg[:,ibin]) #std and avg. over ITERATIONS, per bin
+        # phi_errors[ibin] = np.nanstd(phi_avg[:,ibin])/np.nanmean(phi_avg[:,ibin])
+        # cos1_errors[ibin] = np.nanstd(cos1_avg[:,ibin])/np.nanmean(cos1_avg[:,ibin])
+        # cos2_errors[ibin] = np.nanstd(cos2_avg[:,ibin])/np.nanmean(cos2_avg[:,ibin])
+        # cos3_errors[ibin] = np.nanstd(cos3_avg[:,ibin])/np.nanmean(cos3_avg[:,ibin])
+        #This means bootstrap errors are saved as RELATIVE errors
+        print(f"Cos1 = {np.nanstd(cos1_avg[:,ibin])} / {np.nanmean(cos1_avg[:,ibin])} = {cos1_errors[ibin]}")
+
+        q_errors[ibin] = np.nanstd(q_avg[:,ibin])
+        phi_errors[ibin] = np.nanstd(phi_avg[:,ibin])
+        cos1_errors[ibin] = np.nanstd(cos1_avg[:,ibin])
+        cos2_errors[ibin] = np.nanstd(cos2_avg[:,ibin])
+        cos3_errors[ibin] = np.nanstd(cos3_avg[:,ibin])
+        #This means bootstrap errors are saved as ABSOLUTE errors
+
+    bootstrap_errors = {}
+    bootstrap_errors["q_perp"] = q_errors
+    bootstrap_errors["phi"] = phi_errors
+    bootstrap_errors["cos1"] = cos1_errors
+    bootstrap_errors["cos2"] = cos2_errors
+    bootstrap_errors["cos3"] = cos3_errors
+
+    return bootstrap_errors #RELATIVE Uncertanties
+
+
+#Look at distribution of asymmetry angle inside q_perp Bins
+def phi_inside_qperp(dic, q_perp_bins,phi_bins,q_perp,asymm_phi,weights):
+
+    digits = np.digitize(q_perp,q_perp_bins)-1
+    N_Bins = len(q_perp_bins)-1
+    
+    bin_centers = (phi_bins[:-1]+phi_bins[1:])/2
+    dic["bin_centers"] = bin_centers
+    for i in range(N_Bins):
+        bin_mask = digits==i
+
+        dic[str(i)],_ = np.histogram(asymm_phi[bin_mask],bins=phi_bins,weights=weights[bin_mask],density=True)
+
+    file = open('phi_inside_qperp.pkl', 'wb')
+    pickle.dump(dic, file,protocol=pickle.HIGHEST_PROTOCOL)
+    file.close()
+        
+    return
+
